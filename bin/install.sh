ed2f0df0052887ffbeddbf420e1c8b21c9058ff8
Tyler Ipson <tyler.ipson@clearlink.com>
2020-01-23 09:52:57 -0700

    kill separate install idea, use one script

--- bin/install.sh
+++ bin/install.sh
@@ -1,5 +1,26 @@
 #!/bin/sh
 
-HOME=~/dotfiles
+DF="$HOME/dotfiles"
+VIMDIR="$DF/.vim"
 
-./bin/install-vim.sh
+warn() {
+	echo "$1" >&2
+}
+
+die() {
+	warn "$1"
+	exit 1
+}
+
+# vim
+[ ! -e "$VIMDIR/autoload/plug.vim" ] && eval "curl -fLo $VIMDIR/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
+
+cd ..
+
+ln -s dotfiles/.vim/ .
+ln -s dotfiles/.vim/.vimrc .vimrc
+
+# tmux
+
+ln -s dotfiles/.tmux/ .
+ln -s dotfiles/.tmux.conf .tmux.conf
--- bin/utils.sh
+++
@@ -1,8 +0,0 @@
-warn() {
-	echo "$1" >&2
-}
-
-die() {
-	warn "$1"
-	exit 1
-}
